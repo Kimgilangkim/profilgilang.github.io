0f174124f5eb36cb6b13e1d20059720f9b2a6704
Kimgilangkim <definiteprocess@gmail.com>
2024-09-25 11:17:40 +0700

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>gilang muharam personal Web Site</title>
+</head>
+<body>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="contact.php">Contact</a>
+        <a href="about.php">About</a>
+    </nav>
+
+    <hr />
+
+    <header style="text-align: center">
+        <img src="img/Lang.jpg" width="200" height="200" style="border-radius: 50%">
+        <h1>gilang muharam</h1>
+        <p>(Web Developer Muda)</p>
+    </header>
+
+    <hr />
+
+    <article style="text-align: center;">
+        <h2>Overview</h2>
+        <p>
+            Hi, saya adalah Web Developer muda yang berdomisili di tanggerang selatan.
+            Saat ini sedang belajar HTML untuk membuat profil pribadi.
+        </p>
+    </article>
+    <div style="max-width: 600px margin: 3em auto">
+        <table border="1" width="100%">
+            <thead>
+                <tr>
+                    <th>Skill</th>
+                    <th>Penggalaman</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <ul>
+                            <li>HTML (Expert)</li>
+                            <li>CSS(Beginer)</li>
+                            <li>Javascript (Beginer)
+                            </li>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul>
+                            <li>Freelancer di Internet</li>
+                            <li>Leader Local Linux Comunity</li>
+                            <li>Leader Local Linux Comunity</li>
+                        </ul>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <hr />
+    <footer style="text-align:  center">
+        <p>Copyright &copy; 2024 gilang muharam
+        </p>
+    </footer>
+</body>
+</html>
---
+++ about.php
@@ -0,0 +1,34 @@
+ <!DOCTYPE html>
+ <html lang="en">
+   <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width,initial-scale=1.8">
+     <title>gilang muharam peronal website</title>
+   </head>
+   <body>
+     <nav>
+       <a href="index.php">Home</a> 
+       <a href="content.php">Content</a>
+       <a href="about.php">About me</a>
+     </nav>
+     <hr />
+     <article>
+       <h1>About me</h1>
+       <p>
+         Hi saya adalah web Developer muda yang berdomisili di tengerang selatan,saat ini saya sedang belajar html untuk membuat profile pribadi
+       </p>
+       <p>
+         saya memang masih baru dalam we development,karna itu saya tidak akan pernah berhenti belajar dengan serius
+       </p>
+       <p>
+         saya ingin menguasai dunia eh bahasa html,css,san havascript simak video lengkap tentang saya okyy
+       </p>
+       <p>
+         ISI DENGAN I FRAME
+     </article>
+     <hr>
+     <footer style="text-alinf: center;">
+       <p>Copyright &copy; 2024 gilang muharam</p>
+     </footer>
+   </body>
+ </html>
